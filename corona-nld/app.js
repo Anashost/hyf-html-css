b581afa386a713d5492cfae0030039097324b160
Anashost <41167157+Anashost@users.noreply.github.com>
2020-04-04 21:07:14 +0200

    v1.6

--- corona-nld/app.js
+++ corona-nld/app.js
@@ -1,13 +1,22 @@
-
-$.getJSON("https://api.covid19api.com/summary",function (data) {
+$.getJSON("https://api.covid19api.com/summary", function (data) {
     console.log(data);
+    console.log(data.Countries);
 
+    let findCountry;
+    for (let i = 0; i < data.Countries.length; i++) {
+        if (data.Countries[i].Country === "Netherlands" && data.Countries[i].CountryCode === "NL" && data.Countries[i].Slug === "netherlands") {
+            findCountry = i;
+            break;
+        }
+    }
 
-    let total_confirmed = data.Countries[167].TotalConfirmed;
-    let total_deaths = data.Countries[167].TotalDeaths;
-    let new_confirmed = data.Countries[167].NewConfirmed;
-    let new_deaths = data.Countries[167].NewDeaths;
-    let total_recovered = data.Countries[167].TotalRecovered;
+    console.log(findCountry);
+
+    let total_confirmed = data.Countries[findCountry].TotalConfirmed;
+    let total_deaths = data.Countries[findCountry].TotalDeaths;
+    let new_confirmed = data.Countries[findCountry].NewConfirmed;
+    let new_deaths = data.Countries[findCountry].NewDeaths;
+    let total_recovered = data.Countries[findCountry].TotalRecovered;
 
     $('.total_confirmed').append(total_confirmed);
     $('.total_deaths').append(total_deaths);
@@ -17,7 +26,7 @@ $.getJSON("https://api.covid19api.com/summary",function (data) {
 
 
     var d = new Date();
-    var strDate = d.getFullYear() + "/" + (d.getMonth()+1) + "/" + d.getDate();
+    var strDate = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
     $('.date').append(strDate);
 
 });
