3391a70974d4c46ebcfb8c530ed451795a1faf49
Anashost <41167157+Anashost@users.noreply.github.com>
2020-04-04 20:04:34 +0200

    v1.5

--- corona-nld/app.js
+++ corona-nld/app.js
@@ -3,11 +3,11 @@ $.getJSON("https://api.covid19api.com/summary",function (data) {
     console.log(data);
 
 
-    let total_confirmed = data.Countries[147].TotalConfirmed;
-    let total_deaths = data.Countries[147].TotalDeaths;
-    let new_confirmed = data.Countries[147].NewConfirmed;
-    let new_deaths = data.Countries[147].NewDeaths;
-    let total_recovered = data.Countries[147].TotalRecovered;
+    let total_confirmed = data.Countries[167].TotalConfirmed;
+    let total_deaths = data.Countries[167].TotalDeaths;
+    let new_confirmed = data.Countries[167].NewConfirmed;
+    let new_deaths = data.Countries[167].NewDeaths;
+    let total_recovered = data.Countries[167].TotalRecovered;
 
     $('.total_confirmed').append(total_confirmed);
     $('.total_deaths').append(total_deaths);
