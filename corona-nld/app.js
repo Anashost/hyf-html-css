2e3ecac397554a67b68cc01c1eadc2fcfe9dd098
Anashost <41167157+Anashost@users.noreply.github.com>
2020-04-05 20:04:28 +0200

    v1.7

--- corona-nld/app.js
+++ corona-nld/app.js
@@ -32,8 +32,6 @@
 // });
 
 $.getJSON("https://corona.lmao.ninja/countries/Nl", function (data) {
-    console.log(data);
-
 
     let total_confirmed = data.cases;
     let total_deaths = data.deaths;
@@ -42,14 +40,12 @@ $.getJSON("https://corona.lmao.ninja/countries/Nl", function (data) {
     let total_recovered = data.recovered;
     let flag = data.countryInfo.flag;
 
-
     $('.total_confirmed').append(total_confirmed);
     $('.total_deaths').append(total_deaths);
     $('.total_recovered').append(total_recovered);
     $('.new_confirmed').append(new_confirmed);
     $('.new_deaths').append(new_deaths);
     $('.flag').attr('src',flag);
-
 
     var d = new Date();
     var strDate = d.getFullYear() + "/" + (d.getMonth() + 1) + "/" + d.getDate();
