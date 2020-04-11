222ba4a9bb200321a75e9491fbfaf59c3143b5ee
Anashost <41167157+Anashost@users.noreply.github.com>
2020-04-11 17:14:05 +0200

    Add files via upload

--- corona-nld/app.js
+++ corona-nld/app.js
@@ -35,30 +35,89 @@ $.getJSON("https://corona.lmao.ninja/all", function (data) {
     $('.new_deaths').append(new_deaths);
     $('.flag').attr('src', flag);
 
+
 });
 
-$.getJSON("https://corona.lmao.ninja/countries/" ,function (data) {
+$.getJSON("https://corona.lmao.ninja/countries/", function (data) {
     console.log(data);
 
-
-let default_flag = "0";
-    for (let i = 0; i <data.length ; i++) {
-        console.log(data[i].country);
-        if (data[i].countryInfo.iso2 == null){
+    let default_flag = "0";
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].countryInfo.iso2 == null) {
             data[i].countryInfo.iso2 = default_flag;
         }
+
         let html_code =
-        '<tr>\n' +
-            '        <td><img class="flag-list" src="https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/'+data[i].countryInfo.iso2.toLowerCase()+'.png">'+data[i].country+'</td>' +
-            '        <td>'+data[i].cases+'</td>\n' +
-            '        <td>'+data[i].recovered+'</td>\n' +
-            '        <td>'+data[i].deaths+'</td>\n' +
-            '    </tr>' ;
+            '<tr>\n' +
+            '        <td><img class="flag-list" src="https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/' + data[i].countryInfo.iso2.toLowerCase() + '.png">' + data[i].country + '</td>' +
+            '        <td>' + data[i].cases + '<p id="plus-cases" class="set-red">+ ' + data[i].todayCases + '</p></td>\n' +
+            '        <td>' + data[i].recovered + '</td>\n' +
+            '        <td>' + data[i].deaths + '<p class="set-red">+ ' + data[i].todayDeaths + '</p></td>\n' +
+            '    </tr>';
 
         $(".table-data").append(html_code);
 
-    }
 
+    }
 
 });
 
+
+function myFunction() {
+    let a = document.getElementById("titlex");
+    if (a.innerHTML === "احصائيات كورونا في العالم") {
+        a.innerHTML = "Corona stats in the world";
+    } else {
+        a.innerHTML = "احصائيات كورونا في العالم";
+    }
+
+    let b = document.getElementById("stats");
+    if (b.innerHTML === "مباشر") {
+        b.innerHTML = "Live";
+    } else {
+        b.innerHTML = "مباشر";
+    }
+    let c = document.getElementById("t-country");
+    if (c.innerHTML === "البلد") {
+        c.innerHTML = "Country";
+    } else {
+        c.innerHTML = "البلد";
+    }
+    let d = document.getElementById("all-countries");
+    if (d.innerHTML === "جميع دول العالم") {
+        d.innerHTML = "All Countries";
+    } else {
+        d.innerHTML = "جميع دول العالم";
+    }
+
+    let e = document.getElementById("t-cases");
+    if (e.innerHTML === "جميع الحالات") {
+        e.innerHTML = "Total cases";
+    } else {
+        e.innerHTML = "جميع الحالات";
+    }
+
+    let f = document.getElementById("t-recovered");
+    if (f.innerHTML === "المتعافين") {
+        f.innerHTML = "Total Recovered";
+    } else {
+        f.innerHTML = "المتعافين";
+    }
+
+    let g = document.getElementById("t-deaths");
+    if (g.innerHTML === "الوفيات") {
+        g.innerHTML = "Total Deaths";
+    } else {
+        g.innerHTML = "الوفيات";
+    }
+
+    let h = document.getElementById("change-lang");
+    if (h.innerHTML === "انكليزي") {
+        h.innerHTML = "Arabic";
+    } else {
+        h.innerHTML = "انكليزي";
+    }
+
+    $("#all-ar").toggle();
+    $("#all").toggle();
+}
